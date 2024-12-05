eb475b8177f43ac733cbebbeaf1cefbeacc49b45
Hamza Harti <hamza.harti94@gmail.com>
2024-12-05 13:43:15 +0100

    bug fix Duplicate FlutterActivity

--- android/app/src/main/java/com/DHBW/event_calendar/MainActivity.java
+++ android/app/src/main/java/com/DHBW/event_calendar/MainActivity.java
@@ -1,6 +1,6 @@
 package com.DHBW.event_calendar;
 
-import io.flutter.embedding.android.FlutterActivity;
+//import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
-}
+//public class MainActivity extends FlutterActivity {
+//}
